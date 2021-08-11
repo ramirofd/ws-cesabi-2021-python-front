b436f504cf4adddea7b30637df568e7617b6d8df
Ramiro Detke <ramiro.fer.det@gmail.com>
2021-08-11 18:22:45 -0300

    appbar y pestañas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { createTheme, makeStyles } from "@material-ui/core/styles";
+import {
+  Tab,
+  Tabs,
+  ThemeProvider,
+} from "@material-ui/core";
+
+import "./App.css";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#BF2633",
+    },
+    secondary: {
+      main: "#D9D166",
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  contentContainer: {
+    paddingTop: "120px",
+  },
+  appBar: {
+    textAlign: "center",
+  },
+  fabStyle: {
+    margin: theme.spacing(1),
+    top: "auto",
+    right: 20,
+    bottom: 20,
+    left: "auto",
+    position: "fixed",
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    "aria-controls": `scrollable-force-tabpanel-${index}`,
+  };
+}
 
 function App() {
+  const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <AppBar position="fixed">
+          <Toolbar variant="regular" className={classes.appBar}>
+            <Typography variant="h6">
+              
+            </Typography>
+          </Toolbar>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="on"
+          >
+            {/* Add new tabs here ----------------------------------------------- NEW PROJECTS TABS */}
+            <Tab label="empty" {...a11yProps(0)} />
+          </Tabs>
+        </AppBar>
+      </ThemeProvider>
     </div>
   );
 }
